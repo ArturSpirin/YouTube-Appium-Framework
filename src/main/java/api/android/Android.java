34f2b6f88f83331f1bc00a214510fd832c668735
Artur Spirin <a.spirin@hotmail.com>
2016-04-20 19:36:40 -0700

    YouTube Appium Tutorial - Creating Mobile Automation - Part 3

---
+++ src/main/java/api/android/Android.java
@@ -0,0 +1,11 @@
+package api.android;
+
+import io.appium.java_client.android.AndroidDriver;
+
+/**
+ * Created by Artur on 4/20/2016.
+ */
+public class Android {
+
+    public static AndroidDriver driver;
+}
--- src/main/java/core/UiObject.java
+++ src/main/java/core/UiObject.java
@@ -1,5 +1,10 @@
 package core;
 
+import api.android.Android;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by Artur on 4/13/2016.
  */
@@ -10,5 +15,136 @@ public class UiObject {
     UiObject(String locator){
         this.locator = locator;
         System.out.println(this.locator);
+    }
+
+    private boolean isXpath(){
+        return !locator.contains("UiSelector");
+    }
+
+    public boolean exists(){
+        try{
+            WebElement element;
+            if(isXpath()) element = Android.driver.findElementByXPath(locator);
+            else element = Android.driver.findElementByAndroidUIAutomator(locator);
+            return element.isDisplayed();
+        }catch (NoSuchElementException e){
+            return false;
+        }
+    }
+
+    public boolean isChecked(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("checked").equals("true");
+    }
+
+    public boolean isCheckable(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("checkable").equals("true");
+    }
+
+    public boolean isClickable(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("clickable").equals("true");
+    }
+
+    public boolean isEnabled(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("enabled").equals("true");
+    }
+
+    public boolean isFocusable(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("focusable").equals("true");
+    }
+
+    public boolean isFocused(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("focused").equals("true");
+    }
+
+    public boolean isScrollable(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("scrollable").equals("true");
+    }
+
+    public boolean isLongClickable(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("longClickable").equals("true");
+    }
+
+    public boolean isSelected(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("selected").equals("true");
+    }
+
+    public Point getLocation(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getLocation();
+    }
+
+    public String getText(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("name");
+    }
+
+    public String getResourceId(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("resourceId");
+    }
+
+    public String getClassName(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("className");
+    }
+
+    public String getContentDesc(){
+        WebElement element;
+        if(isXpath()) element = Android.driver.findElementByXPath(locator);
+        else element = Android.driver.findElementByAndroidUIAutomator(locator);
+        return element.getAttribute("contentDesc");
+    }
+
+    public UiObject clearText(){
+        if(isXpath()) Android.driver.findElementByXPath(locator).clear();
+        else  Android.driver.findElementByAndroidUIAutomator(locator).clear();
+        return this;
+    }
+
+    public UiObject typeText(String value){
+        if(isXpath()) Android.driver.findElementByXPath(locator).sendKeys(value);
+        else  Android.driver.findElementByAndroidUIAutomator(locator).sendKeys(value);
+        return this;
+    }
+
+    public UiObject tap(String value){
+        if(isXpath()) Android.driver.findElementByXPath(locator).click();
+        else  Android.driver.findElementByAndroidUIAutomator(locator).click();
+        return this;
     }
 }
