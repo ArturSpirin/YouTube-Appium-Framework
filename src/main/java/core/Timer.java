242263d914055801ce2abcdbaa0e24975218cfee
Artur Spirin <a.spirin@hotmail.com>
2016-05-03 08:08:52 -0700

    YouTube Appium Tutorial - Creating Mobile Automation Framework - Part 4

---
+++ src/main/java/core/Timer.java
@@ -0,0 +1,24 @@
+package core;
+
+import java.util.Date;
+
+/**
+ * Created by Artur on 4/22/2016.
+ */
+public class Timer {
+
+    public long startStamp;
+
+    public void start(){
+        startStamp = getTimeStamp();
+    }
+
+    public static long getTimeStamp(){
+        return new Date().getTime();
+    }
+
+    public boolean expired(int seconds){
+        int difference = (int) ((getTimeStamp() - startStamp) / 1000);
+        return difference > seconds;
+    }
+}
--- src/main/java/core/UiObject.java
+++ src/main/java/core/UiObject.java
@@ -142,9 +142,37 @@ public class UiObject {
         return this;
     }
 
-    public UiObject tap(String value){
+    public UiObject tap(){
         if(isXpath()) Android.driver.findElementByXPath(locator).click();
         else  Android.driver.findElementByAndroidUIAutomator(locator).click();
+        return this;
+    }
+
+    public UiObject scrollTo(){
+        if(!locator.contains("text")) throw new RuntimeException("Scroll to method can only be used with text attributes and current locator: "+locator+" does not contain any text attributes!");
+        if(isXpath()) Android.driver.scrollTo(locator.substring(locator.indexOf("@text=\""), locator.indexOf("\"]")).replace("@text=\"", ""));
+        else{
+            String text;
+            if(locator.contains("textContains")) text = locator.substring(locator.indexOf(".textContains(\""), locator.indexOf("\")")).replace(".textContains(\"", "");
+            else text = locator.substring(locator.indexOf(".text(\""), locator.indexOf("\")")).replace(".text(\"", "");
+            Android.driver.scrollTo(text);
+        }
+        return this;
+    }
+
+    public UiObject waitToAppear(int seconds){
+        Timer timer = new Timer();
+        timer.start();
+        while(!timer.expired(seconds)) if(exists()) break;
+        if(timer.expired(seconds) && !exists()) throw new AssertionError("Element "+locator+" failed to appear within "+seconds+" seconds");
+        return this;
+    }
+
+    public UiObject waitToDisappear(int seconds){
+        Timer timer = new Timer();
+        timer.start();
+        while(!timer.expired(seconds)) if(!exists()) break;
+        if(timer.expired(seconds) && exists()) throw new AssertionError("Element "+locator+" failed to disappear within "+seconds+" seconds");
         return this;
     }
 }
