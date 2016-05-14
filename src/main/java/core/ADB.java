61a9f837170aed93cb484aaf2e4f61f49623d17e
Artur Spirin <a.spirin@hotmail.com>
2016-05-14 08:04:13 -0700

    YouTube Appium Tutorial - Creating Mobile Automation Framework - Part 7

--- src/main/java/core/ADB.java
+++ src/main/java/core/ADB.java
@@ -1,7 +1,9 @@
 package core;
 import core.managers.ServerManager;
+import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Artur on 4/13/2016.
@@ -10,9 +12,7 @@ public class ADB {
 
     private String ID;
 
-    public ADB(String deviceID){
-        ID = deviceID;
-    }
+    //public ADB(String deviceID){ID = deviceID;}
 
     public static String command(String command){
         if(command.startsWith("adb")) command = command.replace("adb ", ServerManager.getAndroidHome()+"/platform-tools/adb ");
@@ -119,5 +119,58 @@ public class ADB {
 
     public String getDeviceCarrier(){
         return command("adb -s "+ID+" shell getprop gsm.operator.alpha");
+    }
+
+    public ArrayList getLogcatProcesses(){
+        String[] output = command("adb -s "+ID+" shell top -n 1 | grep -i 'logcat'").split("\n");
+        ArrayList processes = new ArrayList();
+        for(String line : output){
+            processes.add(line.split(" ")[0]);
+            processes.removeAll(Arrays.asList("", null));
+        }
+        return processes;
+    }
+
+    public Object startLogcat(String logID, String grep){
+        ArrayList pidBefore = getLogcatProcesses();
+
+        Thread logcat = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(grep == null) command("adb -s "+ID+" shell logcat -v threadtime > /sdcard/"+logID+".txt");
+                else command("adb -s "+ID+" shell logcat -v threadtime | grep -i '"+grep+"'> /sdcard/"+logID+".txt");
+            }
+        });
+        logcat.setName(logID);
+        logcat.start();
+        logcat.interrupt();
+
+        ArrayList pidAfter = getLogcatProcesses();
+        Timer timer = new Timer();
+        timer.start();
+        while(!timer.expired(5)){
+            if(pidBefore.size() > 0) pidAfter.removeAll(pidBefore);
+            if(pidAfter.size() > 0) break;
+            pidAfter = getLogcatProcesses();
+        }
+
+        if(pidAfter.size() == 1) return pidAfter.get(0);
+        else if(pidAfter.size() > 1) throw new RuntimeException("Multiple logcat processes were started when only one was expected!");
+        else throw new RuntimeException("Failed to start logcat process!");
+    }
+
+    public void stopLocat(Object PID){
+        command("adb -s "+ID+" shell kill "+PID);
+    }
+
+    @Test
+    public void test(){
+        ID = "00a0493f4f06c6d4";
+        System.out.println("Processes Prior to starting new logcat: "+getLogcatProcesses());
+        Object PID = startLogcat("1", null);
+        System.out.println("Started logcat on PID: "+PID);
+        System.out.println("Processes after starting new logcat: "+getLogcatProcesses());
+        stopLocat(PID);
+        System.out.println("Processes after stopping new logcat: "+getLogcatProcesses());
     }
 }
